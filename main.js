c00861327126297e749efabfbf35ecafb4ef51d1
SamerSenbol <samer.senbol@gmail.com>
2019-02-28 14:02:04 +0100

    Adding jQuary animate.

--- main.js
+++ main.js
@@ -7,7 +7,10 @@ $(function(){
     // Smoothly Scroll To Element
     $('.navbar li a ').click(function(e){
         e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('#' + $(this).data('scroll')).offset().top
+        },1000);
 
-        window.console.log($(this).data('scroll'));
+        //window.console.log('#' + $(this).data('scroll'));
     });
 });
