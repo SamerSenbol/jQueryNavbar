9107a810334181931e44f15f53d2f998f47559af
SamerSenbol <samer.senbol@gmail.com>
2019-02-28 15:15:46 +0100

    Calculate body padding.

--- main.js
+++ main.js
@@ -4,13 +4,15 @@ $(function(){
 
     'use strict';
 
+    //Calculate body Padding Top Depend On Navbar Height
+
+    $('body').css('paddingTop', $('.navbar').innerHeight() + 11);
+
     // Smoothly Scroll To Element
     $('.navbar li a ').click(function(e){
         e.preventDefault();
         $('html, body').animate({
             scrollTop: $('#' + $(this).data('scroll')).offset().top
         },1000);
-
-        //window.console.log('#' + $(this).data('scroll'));
     });
 });
