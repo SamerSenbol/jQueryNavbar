18cf21a0d21fa3892fbe8f9d6b955e7077e84a19
SamerSenbol <samer.senbol@gmail.com>
2019-02-28 13:40:42 +0100

    Add un ordar list and jQuary function.

--- main.js
+++ main.js
@@ -5,4 +5,9 @@ $(function(){
     'use strict';
 
     // Smoothly Scroll To Element
+    $('.navbar li a ').click(function(e){
+        e.preventDefault();
+
+        window.console.log($(this).data('scroll'));
+    });
 });
