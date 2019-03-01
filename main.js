d1eb15bea7b74688a8a2ebc3b6ae3641ac32d4f5
SamerSenbol <samer.senbol@gmail.com>
2019-03-01 12:24:59 +0100

    Fixing the syntax spaces.

--- main.js
+++ main.js
@@ -50,10 +50,9 @@ $(function(){
 
     // Click On Scroll To Top To Go Up
     $('.scroll-to-top').click(function(event){
-            event.preventDefault();
-            $('html, body').animate({
-                scrollTop: 0
-            },1000);
-
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        },1000);
     })
 });
