8988050424c6a005027b4f4182cbcb68257d80ef
SamerSenbol <samer.senbol@gmail.com>
2019-03-01 13:51:57 +0100

    Making jQaury function to my popup.

--- main.js
+++ main.js
@@ -54,5 +54,23 @@ $(function(){
         $('html, body').animate({
             scrollTop: 0
         },1000);
-    })
+    });
+
+    // Popup
+    $('.show-popup').click(function(){
+        $('.popup').fadeIn();
+    });
+
+    $('.popup').click(function(){
+        $(this).fadeOut();
+    });
+
+    $('.popup .inner').click(function(e){
+       e.stopPropagation();
+    });
+
+    $('.popup .close').click(function(){
+       $('.popup').fadeOut();
+    });
+
 });
